8ae8e32bf1690fc73297922ca101ba06d78dba55
Mikhail Starilov <starilov.m@gmail.com>
2021-02-17 01:17:28 +0600

    PostmanEchoTest edited

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -12,13 +12,51 @@ public class PostmanEchoTest {
         // Given - When - Then
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\n" +
+                        "  \"store\": {\n" +
+                        "    \"book\": [\n" +
+                        "      {\n" +
+                        "        \"author\": \"Nigel Rees\",\n" +
+                        "        \"category\": \"reference\",\n" +
+                        "        \"price\": 8.95,\n" +
+                        "        \"title\": \"Sayings of the Century\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"author\": \"Evelyn Waugh\",\n" +
+                        "        \"category\": \"fiction\",\n" +
+                        "        \"price\": 12.99,\n" +
+                        "        \"title\": \"Sword of Honour\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"author\": \"Herman Melville\",\n" +
+                        "        \"category\": \"fiction\",\n" +
+                        "        \"isbn\": \"0-553-21311-3\",\n" +
+                        "        \"price\": 8.99,\n" +
+                        "        \"title\": \"Moby Dick\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"author\": \"J. R. R. Tolkien\",\n" +
+                        "        \"category\": \"fiction\",\n" +
+                        "        \"isbn\": \"0-395-19395-8\",\n" +
+                        "        \"price\": 22.99,\n" +
+                        "        \"title\": \"The Lord of the Rings\"\n" +
+                        "      }\n" +
+                        "    ]\n" +
+                        "  }\n" +
+                        "}")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("some value"))
+                .body("data.store.book.size()", equalTo(4))
+                .body("data.store.isEmpty()", is(false))
+                .body("data.store.book.last().price.toInteger()", greaterThan(20))
+                .body("data.store.book[1].price.toString()", is("12.99"))
+                .body("data.store.book.find {it.author =='Nigel Rees'} != null", is(true))
+                .body("data.store.book.every { it.price >= 0 }", is(true))
         ;
     }
 }
