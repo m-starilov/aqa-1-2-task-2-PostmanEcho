d2cfa3622a68b9a2939c50d59f6fb959b84e6848
Mikhail Starilov <starilov.m@gmail.com>
2021-02-16 23:08:20 +0600

    Initial commit

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.postman;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnBody() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
